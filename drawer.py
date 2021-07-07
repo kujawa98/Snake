cd06dfe3c414e17eadc4238d41d05f2b1383c489
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-07-07 10:27:43 +0200

    Added background tile map

--- drawer.py
+++ drawer.py
@@ -4,13 +4,16 @@ import pygame
 
 def draw_snake(snake, food):
     window.fill(BLACK)
+    window.blit(BGC, (0, 0))
     window.blit(IMG, (OFFSET_X, OFFSET_Y))
     for part in snake.parts:
-        pygame.draw.rect(window, WHITE,
+        pygame.draw.rect(window, BLACK,
                          (part.x * PART_WIDTH + OFFSET_X, part.y * PART_HEIGHT + OFFSET_Y, PART_WIDTH, PART_HEIGHT))
     draw_food(food)
 
 
 def draw_food(food):
-    pygame.draw.rect(window, LIMON,
-                     (food[0] * PART_WIDTH + OFFSET_X, food[1] * PART_HEIGHT + OFFSET_Y, PART_WIDTH, PART_HEIGHT))
+    pygame.draw.circle(window, RED,
+                       (food[0] * PART_WIDTH + OFFSET_X + PART_WIDTH // 2,
+                        food[1] * PART_HEIGHT + OFFSET_Y + PART_WIDTH // 2),
+                       PART_WIDTH // 2)
--- setup.py
+++ setup.py
@@ -8,21 +8,21 @@ BOARD_HEIGHT = 20
 pygame.init()
 
 # window properties
-WINDOW_WIDTH, WINDOW_HEIGHT = 1000, 1000
-INNER_WIDTH, INNER_HEIGHT = 272, 320
+WINDOW_WIDTH, WINDOW_HEIGHT = 1024, 1024
+INNER_WIDTH, INNER_HEIGHT = 544, 640
 OFFSET_X = (WINDOW_WIDTH - INNER_WIDTH) // 2
 OFFSET_Y = (WINDOW_HEIGHT - INNER_HEIGHT) // 2
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Snek')
 
 # colors
-BLUE = (65, 105, 225)
+RED = (255, 0, 0)
 WHITE = (255, 255, 255)
-LIMON = (147, 246, 0)
 BLACK = (0, 0, 0)
 
 # others
 FPS = 60
-PART_WIDTH, PART_HEIGHT = 16, 16
+PART_WIDTH, PART_HEIGHT = 32, 32
 PARTS_ON_START = 3
 IMG = pygame.image.load("assets/snek.png")
+BGC = pygame.image.load("assets/bcg.png")
