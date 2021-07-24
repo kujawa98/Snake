156bb71b80ab99532f9ad3ee25394ca69924b005
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-07-24 18:32:38 +0200

    Add background image and set fullscreen

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
+import pygame
+
 DIRECTIONS = {"up": (0, -1), "down": (0, 1), "left": (-1, 0), "right": (1, 0)}
 BOARD_WIDTH = 17
 BOARD_HEIGHT = 20
 
-
 # window properties
-WINDOW_WIDTH, WINDOW_HEIGHT = 544, 640
+WINDOW_WIDTH, WINDOW_HEIGHT = 1920, 1080
+INNER_WIDTH, INNER_HEIGHT = 544, 640
+OFFSET_X = (WINDOW_WIDTH - INNER_WIDTH) // 2
+OFFSET_Y = (WINDOW_HEIGHT - INNER_HEIGHT) // 2
 
 # colors
 WHITE = (255, 255, 255)
@@ -14,3 +18,5 @@ BLACK = (0, 0, 0)
 FPS = 60
 PART_WIDTH, PART_HEIGHT = 32, 32
 PARTS_ON_START = 3
+
+BG_IMG = pygame.image.load('./assets/snake_bg_img.png')
