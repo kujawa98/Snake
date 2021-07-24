6b3cf707c62a9a286595fd754770db9b2402b3df
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-07-24 10:53:20 +0200

    Change collision detection codition

--- collision_checker.py
+++ collision_checker.py
@@ -2,8 +2,8 @@ from setup import BOARD_WIDTH, BOARD_HEIGHT
 
 
 def check_collision(snake):
-    if snake.head.x + snake.direction[0] <= 0 or snake.head.x + snake.direction[0] >= BOARD_WIDTH or snake.head.y + \
-            snake.direction[1] <= 0 or snake.head.y + snake.direction[1] >= BOARD_HEIGHT:
+    if snake.head.x + snake.direction[0] < 0 or snake.head.x + snake.direction[0] >= BOARD_WIDTH or snake.head.y + \
+            snake.direction[1] < 0 or snake.head.y + snake.direction[1] >= BOARD_HEIGHT:
         return True
     for part in snake.parts:
         if part != snake.head and part.x == snake.head.x and part.y == snake.head.y:
